6d8c925c4b7301e0ec2d60f9513881d45a4fd049
Niels Hoffmann <niels@zentralmaschine.net>
2024-12-07 03:02:55 +0100

    Add a zero const

--- src/vectors.rs
+++ src/vectors.rs
@@ -1,5 +1,7 @@
 use std::ops::{Add, AddAssign, Div, Mul, Sub, SubAssign};
 
+use bevy::math::Vec2;
+
 #[derive(Copy, Clone, Debug, Default, PartialEq, Eq, Hash)]
 pub struct Vector2Int {
     pub x: i32,
@@ -11,6 +13,7 @@ impl Vector2Int {
     pub const DOWN: Vector2Int = Vector2Int { x: 0, y: -1 };
     pub const LEFT: Vector2Int = Vector2Int { x: -1, y: 0 };
     pub const RIGHT: Vector2Int = Vector2Int { x: 1, y: 0 };
+    pub const ZERO: Vector2Int = Vector2Int { x: 0, y: 0 };
 
     pub fn new(x: i32, y: i32) -> Self {
         Self { x, y }
@@ -72,5 +75,11 @@ impl Mul<Vector2Int> for i32 {
 
     fn mul(self, other: Vector2Int) -> Vector2Int {
         return Vector2Int::new(other.x * self, other.y * self);
+    }
+}
+
+impl From<Vec2> for Vector2Int {
+    fn from(v: Vec2) -> Self {
+        Vector2Int::new(v.x as i32, v.y as i32)
     }
 }
